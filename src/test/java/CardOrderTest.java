350afb1a8ac69e1d4570ed07a935dc87af185692
Angelina <angry.anggri@gmail.com>
2023-06-24 17:25:30 +0300

    commit

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class CardOrderTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class CardOrderTest {
+
+    private WebDriverManager driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTest() {
+        throw new UnsupportedOperationException();
+    }
 }
